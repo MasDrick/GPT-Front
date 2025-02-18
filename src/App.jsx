165625a6d98dc195ea5801359a87e47f80ffc355
MasDrick <maksimbelyh05@gmail.com>
2025-02-18 18:05:10 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="wrap">
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>В разработке ...</h1>
+      {/* <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div> */}
+      <p className="read-the-docs">Пните Масдрика, чтобы он пилил фронт</p>
+    </div>
+  );
+}
+
+export default App;
