f5338669fae029c895e5dad027e7259f9dc03782
MasDrick <maksimbelyh05@gmail.com>
2025-02-27 21:50:08 +0500

    add safearea

--- src/App.jsx
+++ src/App.jsx
@@ -20,53 +20,14 @@ function App() {
     },
   });
 
-  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
-
   useEffect(() => {
     tg.ready();
     tg.expand();
-
-    const handleResize = () => {
-      const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
-      let isKeyboardVisible = false;
-
-      if (isIOS && window.visualViewport) {
-        isKeyboardVisible = window.visualViewport.height < window.innerHeight;
-      } else {
-        isKeyboardVisible = window.innerHeight < screen.height;
-      }
-
-      setIsKeyboardOpen(isKeyboardVisible);
-
-      if (isKeyboardVisible) {
-        document.body.classList.add('keyboard-open');
-      } else {
-        document.body.classList.remove('keyboard-open');
-      }
-    };
-
-    const handleFocus = (event) => {
-      if (isKeyboardOpen) {
-        setTimeout(() => {
-          event.target.scrollIntoView({ behavior: 'smooth', block: 'center' });
-        }, 300);
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-    document.addEventListener('focusin', handleFocus); // Работает при фокусе на инпуте
-    document.addEventListener('focusout', handleResize); // При скрытии клавиатуры
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-      document.removeEventListener('focusin', handleFocus);
-      document.removeEventListener('focusout', handleResize);
-    };
-  }, [isKeyboardOpen]);
+  }, []);
 
   return (
     <ConfigProvider theme={theme}>
-      <div id="app" className={`app ${isKeyboardOpen ? 'keyboard-open' : ''}`}>
+      <div id="app" className="app">
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
