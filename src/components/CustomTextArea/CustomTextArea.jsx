f5a42845d2d5f8dd2407879c9ac21a1d2b183c20
lilbig <ivansherstobitov456@gmail.com>
2025-03-01 18:03:04 +0500

    keyboard

--- src/components/CustomTextArea/CustomTextArea.jsx
+++ src/components/CustomTextArea/CustomTextArea.jsx
@@ -1,27 +1,28 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { useTelegram } from '../../hooks/useTelegram';
-import { Paperclip, ArrowUp } from 'lucide-react';
-import styles from './CustomTextArea.module.scss';
-import { useAtom } from 'jotai';
-import { chatHistoryAtom, activeModelAI } from '../../store/atoms';
-import axios from 'axios';
-import useTelegramViewportHack from '../../hooks/useTelegramViewportHack';
+import React, { useRef, useEffect, useState } from "react";
+import { useTelegram } from "../../hooks/useTelegram";
+import { Paperclip, ArrowUp } from "lucide-react";
+import styles from "./CustomTextArea.module.scss";
+import { useAtom } from "jotai";
+import { chatHistoryAtom, activeModelAI } from "../../store/atoms";
+import axios from "axios";
+import useTelegramViewportHack from "../../hooks/useTelegramViewportHack";
 
 const CustomTextArea = ({ placeholder }) => {
   const [active, setActive] = useState(false);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const containerRef = useRef(null);
   const textareaRef = useRef(null);
   const [chatHistory, setChatHistory] = useAtom(chatHistoryAtom);
   const [model] = useAtom(activeModelAI);
   const { tg, queryId, urlBack } = useTelegram();
 
-  const { isKeyboardOpen, keyboardHeight } = useTelegramViewportHack(textareaRef);
+  const { isKeyboardOpen, keyboardHeight } =
+    useTelegramViewportHack(textareaRef);
 
   useEffect(() => {
     const adjustTextareaHeight = () => {
       if (textareaRef.current) {
-        textareaRef.current.style.height = 'auto';
+        textareaRef.current.style.height = "auto";
         textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
       }
     };
@@ -29,8 +30,14 @@ const CustomTextArea = ({ placeholder }) => {
     adjustTextareaHeight();
   }, [message]);
 
-  const textModels = ['gpt-4o', 'gpt-4o-mini', 'deepseek-r1', 'deepseek-v3', 'evil'];
-  const imageModels = ['midjourney', 'dall-e-3', 'flux'];
+  const textModels = [
+    "gpt-4o",
+    "gpt-4o-mini",
+    "deepseek-r1",
+    "deepseek-v3",
+    "evil",
+  ];
+  const imageModels = ["midjourney", "dall-e-3", "flux"];
 
   // Функция определения пути
   const getApiUrl = (model, prompt, userId) => {
@@ -39,17 +46,17 @@ const CustomTextArea = ({ placeholder }) => {
     } else if (imageModels.includes(model)) {
       return `${urlBack}/generate_image/?prompt=${prompt}&user_id=${userId}`;
     } else {
-      throw new Error('Неизвестная модель');
+      throw new Error("Неизвестная модель");
     }
   };
 
   const handleSubmit = () => {
-    if (message.trim() === '') return;
+    if (message.trim() === "") return;
     const apiUrl = getApiUrl(model, message, queryId);
 
-    setChatHistory((prev) => [...prev, { type: 'user', text: message }]);
-    setMessage('');
-    textareaRef.current.style.height = 'auto';
+    setChatHistory((prev) => [...prev, { type: "user", text: message }]);
+    setMessage("");
+    textareaRef.current.style.height = "auto";
 
     axios
       .post(apiUrl)
@@ -59,14 +66,17 @@ const CustomTextArea = ({ placeholder }) => {
 
         setChatHistory((prev) => [
           ...prev,
-          { type: 'bot', text: botResponse || '', image: imageUrl || '' }, // Добавляем картинку
+          { type: "bot", text: botResponse || "", image: imageUrl || "" }, // Добавляем картинку
         ]);
       })
       .catch((error) => {
-        console.error('Ошибка при получении ответа:', error);
+        console.error("Ошибка при получении ответа:", error);
         setChatHistory((prev) => [
           ...prev,
-          { type: 'bot', text: 'Произошла ошибка. Пожалуйста, попробуйте позже.' },
+          {
+            type: "bot",
+            text: "Произошла ошибка. Пожалуйста, попробуйте позже.",
+          },
         ]);
       });
   };
@@ -74,12 +84,15 @@ const CustomTextArea = ({ placeholder }) => {
   return (
     <div
       ref={containerRef}
-      style={{ marginBottom: isKeyboardOpen ? `${keyboardHeight / 2}px` : '0' }}
-      className={`${styles.custom_textarea} ${active ? styles.active : ''}`}
+      style={{
+        marginBottom: isKeyboardOpen ? `${(keyboardHeight * 0, 3)}px` : "0",
+      }}
+      className={`${styles.custom_textarea} ${active ? styles.active : ""}`}
       onClick={(e) => {
         e.stopPropagation();
         setActive(true);
-      }}>
+      }}
+    >
       <textarea
         ref={textareaRef}
         placeholder={placeholder || `Спросить у ${model}`}
@@ -89,7 +102,7 @@ const CustomTextArea = ({ placeholder }) => {
         onBlur={() => setActive(false)}
         onChange={(e) => setMessage(e.target.value)}
         onKeyDown={(e) => {
-          if (e.key === 'Enter' && !e.shiftKey) {
+          if (e.key === "Enter" && !e.shiftKey) {
             e.preventDefault();
             handleSubmit();
           }
@@ -98,17 +111,21 @@ const CustomTextArea = ({ placeholder }) => {
       <div className={styles.buttons}>
         <button
           className={
-            tg.themeParams?.bg_color === '#ffffff' ? `${styles.btn} ${styles.isLight}` : styles.btn
-          }>
-          <Paperclip color={'#ffffff'} size={18} />
+            tg.themeParams?.bg_color === "#ffffff"
+              ? `${styles.btn} ${styles.isLight}`
+              : styles.btn
+          }
+        >
+          <Paperclip color={"#ffffff"} size={18} />
         </button>
 
         <button
-          className={`${message === '' ? styles.btn : `${styles.btn} ${styles.activeBtn}`} ${
-            tg.themeParams?.bg_color === '#ffffff' ? styles.isLight : ''
-          }`}
-          onClick={handleSubmit}>
-          <ArrowUp color={'#ffffff'} size={18} />
+          className={`${
+            message === "" ? styles.btn : `${styles.btn} ${styles.activeBtn}`
+          } ${tg.themeParams?.bg_color === "#ffffff" ? styles.isLight : ""}`}
+          onClick={handleSubmit}
+        >
+          <ArrowUp color={"#ffffff"} size={18} />
         </button>
       </div>
     </div>
